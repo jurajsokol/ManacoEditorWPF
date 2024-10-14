3b01219c84f258efad458fdcaa7e49ff753a0537
Juraj Sokol <jurajsokol.s@gmail.com>
2024-10-14 21:23:26 +0200

    Add Button for theme change

--- MonacoEditor.SampleApp/MainWindowViewModel.cs
+++ MonacoEditor.SampleApp/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MonacoEditor.WPF.Enums;
 using System.IO;
 
 namespace MonacoEditor.SampleApp;
@@ -7,6 +8,12 @@ public partial class MainWindowViewModel : ObservableObject
 {
     [ObservableProperty]
     private string text = "test";
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(MonacoTheme))]
+    private bool isDarkTheme;
+
+    public MonacoTheme MonacoTheme => IsDarkTheme ? MonacoTheme.Dark : MonacoTheme.Light;
 
     public async Task OpenFromFile(string path)
     {
